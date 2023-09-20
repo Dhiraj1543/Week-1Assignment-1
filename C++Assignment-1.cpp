9222dd748952b0d3db7516cb2a33cadc90db8761
Dhiraj1543 <140428865+Dhiraj1543@users.noreply.github.com>
2023-09-20 16:32:30 +0530

    Solved Assignments Files are added.

---
+++ C++Assignment-1.cpp
@@ -0,0 +1,50 @@
+                    C++ Assignments -1
+
+// Q.no 1.How can you output “Physics” and “Wallah” in two different lines in C++?
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Physcis"<<endl<<"Wallah";
+}
+
+// Q.no 2.Write a program to print 10 using 2 positive numbers less than 6 in C++ as output. 
+  #include<iostream>
+  using namespace std; 
+  int main () { 
+  cout<<5+5;
+   }
+
+// Q.no 3.How much space does the following data types take? 
+int 
+bool 
+float 
+Solution:- 4,1,4
+
+// Q.no 4.What is the output of this program? 
+int main() { 
+ int a = 4; 
+ int b = 5; 
+ a++, b--; 
+ cout << ++a << " " << b--; 
+} 
+Solution:-6 4
+
+// Q.no 5.Write a program to find the circumference of a circle with radius 10 in C++. 
+
+#include<iostream>
+using namespace std;
+int main() { 
+int radius=10;
+float circumference=2*3.14*radius;
+cout<<circumference;
+}
+
+//Q.no 6.How many of these can be a variable name ? 
+01Pwskills 
+_FLOAT 
+int 
+FLOAT 
+You will succeed 
+Solution:- 2 4
+
